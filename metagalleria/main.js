b523582be32b970a793ef1b202ff443e098108ea
livioaldo46inresonantia <quilismaspazioarte@gmail.com>
2026-08-17 12:04:50 +0200

    Update third eye sphere center coordinates
    Adjusted the position of the third eye sphere by modifying its center coordinates.

--- metagalleria/main.js
+++ metagalleria/main.js
@@ -574,11 +574,10 @@ addTree(2.4, -12.4, 1.0);
 
 // Terzo Occhio - sfera di prova
 const thirdEyeCenter = new THREE.Vector3(
-  (P[9].x + P.I1.x + P.I2.x + P.I3.x) / 4,
-  1.65,
-  (P[9].z + P.I1.z + P.I2.z + P.I3.z) / 4
+  P[9].x,
+  2.15,
+  P[9].z
 );
-
 const thirdEyeSphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.65, 48, 32),
   new THREE.MeshStandardMaterial({
