59f1c78e1d4cf49f6affe0e9948d273a5bb6ac2b
livioaldo46inresonantia <quilismaspazioarte@gmail.com>
2026-09-15 22:27:03 +0200

    Adjust camera position and add lookAt method
    Updated camera position for better scene view.

--- metagalleria/main.js
+++ metagalleria/main.js
@@ -5,7 +5,8 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x01031a);
 
 const camera = new THREE.PerspectiveCamera(65, innerWidth / innerHeight, 0.05, 500);
-camera.position.set(0, 1.70, 8.8);
+camera.position.set(0, 1.70, 11.75);
+camera.lookAt(0, 1.70, 0);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
 renderer.setSize(innerWidth, innerHeight);
