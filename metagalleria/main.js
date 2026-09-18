400ba79c509917c55a504c3d9b88d886813e0cf9
livioaldo46inresonantia <quilismaspazioarte@gmail.com>
2026-09-18 22:53:51 +0200

    Update main.js

--- metagalleria/main.js
+++ metagalleria/main.js
@@ -779,7 +779,7 @@ addPanelFaceImage(
 // D18 — segmento I13-I18
 addPanelFaceImage(
   'I13','I18',
-'./INTESTAZIONE_FORTUN_GIALLO_ROSSO_SANGUE.png'
+  './INTESTAZIONE_FORTUN_GIALLO_ROSSO_SANGUE.png',
   {panelWidth:3.30,panelHeight:3.00,face:'opposite'}
 );
 // S18 — DISEGNO BIANCO SU ANTRACITE — segmento I13-I18
