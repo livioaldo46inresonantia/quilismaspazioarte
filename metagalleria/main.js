8737efc0f9ab7943387dce3cdb2f00f732b2babd
livioaldo46inresonantia <quilismaspazioarte@gmail.com>
2026-09-22 06:32:02 +0200

    Update image path for PALEOCAPA 9 panel

--- metagalleria/main.js
+++ metagalleria/main.js
@@ -736,7 +736,7 @@ addPanelFaceImage(
 // D13 — PALEOCAPA 9
 addPanelFaceImage(
   'I17','I9',
-  './PANNELLO_D13_PALEOCAPA_9_CENTRATO_SCRITTA_STANDARD.jpg',
+  './D13.jpg',
   {panelWidth:4.40,panelHeight:3.00,face:'opposite'}
 );
 // D4 — PALEOCAPA — segmento 7-I6
